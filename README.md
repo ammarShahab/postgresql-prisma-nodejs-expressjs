06201060a3ca5e97458b2589d8dad08d38677fe3
ammarShahab <ashahab007@gmail.com>
2026-06-10 21:55:20 +0600

    docs: update README and watchListRoutes with authentication middleware instructions

--- README.md
+++ README.md
@@ -147,4 +147,14 @@ Flow:
 
 13.8 Now to Check pass the following data
 
-<!-- todo: start tutorial from 1:47:40 -->
+14.0 created a middleware folder and make a authMiddleware.js file. Middleware in Express (and web frameworks in general) are functions that run between the incoming request and your route handler. They process the request before it reaches your controller.
+
+14.1 read the token from the request
+
+14.2 varify the token and extract the user id
+
+14.3 use the middleware on specific protected route
+
+14.4 Now from this stage using thunderclient first login or register with the user and then copy the token and paste it in the header of thunderclient http://localhost:5000/watchlist route "Key: Authorization Value: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImQ4ZWJhODA4LTQ0YWUtNGZkNC04ZTIzLWNkNGVhNmI4NDlmMCIsImlhdCI6MTc4MTEwMDIwNCwiZXhwIjoxNzgxNzA1MDA0fQ.fH8SanmV2dKlPMKTfv3KlcsSRPWsiB4urMMcK7DumWY". Now copy any "movieId": "01KTNGFVB9APHBQAAMRRF9ZHNW", "status": "PLANNED", "note": "Good", "rating": 9 and paste it in the body of thunderclient http://localhost:5000/watchlist route and send, the data will be saved in the watchlist in the database. But if u untick the Header from the thunderclient and send the same data, it will show error because the route is protected and we need to pass the token in the header to access the route.
+
+<!-- todo: start tutorial from 2:03:0 -->
--- src/routes/watchListRoutes.js
+++ src/routes/watchListRoutes.js
@@ -9,7 +9,7 @@ const router = express.Router();
 // 14.3 use the middleware on specific protected route
 router.use(authMiddleware);
 
-// 14.4 Now from this stage using thunderclient first login or register with the user and then copy the token and paste it in the header of thunderclient http://localhost:5000/watchlist route "Key: Authorization Value: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImQ4ZWJhODA4LTQ0YWUtNGZkNC04ZTIzLWNkNGVhNmI4NDlmMCIsImlhdCI6MTc4MTEwMDIwNCwiZXhwIjoxNzgxNzA1MDA0fQ.fH8SanmV2dKlPMKTfv3KlcsSRPWsiB4urMMcK7DumWY". Now copy any  "movieId": "01KTNGFVB9APHBQAAMRRF9ZHNW", "status": "PLANNED", "note": "Good", "rating": 9  and paste it in the body of thunderclient http://localhost:5000/watchlist route and send, the data will be saved in the watchlist in the database.
+// 14.4 Now from this stage using thunderclient first login or register with the user and then copy the token and paste it in the header of thunderclient http://localhost:5000/watchlist route "Key: Authorization Value: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImQ4ZWJhODA4LTQ0YWUtNGZkNC04ZTIzLWNkNGVhNmI4NDlmMCIsImlhdCI6MTc4MTEwMDIwNCwiZXhwIjoxNzgxNzA1MDA0fQ.fH8SanmV2dKlPMKTfv3KlcsSRPWsiB4urMMcK7DumWY". Now copy any "movieId": "01KTNGFVB9APHBQAAMRRF9ZHNW", "status": "PLANNED", "note": "Good", "rating": 9 and paste it in the body of thunderclient http://localhost:5000/watchlist route and send, the data will be saved in the watchlist in the database. But if u untick the Header from the thunderclient and send the same data, it will show error because the route is protected and we need to pass the token in the header to access the route.
 
 router.post("/", addToWatchList);
 
